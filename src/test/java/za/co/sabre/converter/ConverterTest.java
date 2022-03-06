4001963c16acc31ed9dfe7a63394801ea9cb3bec
Sabre Didi <sabre@red-i.co.za>
2022-03-06 10:32:54 +0200

    added the intergration tests

--- src/test/java/za/co/sabre/converter/ConverterTest.java
+++ src/test/java/za/co/sabre/converter/ConverterTest.java
@@ -2,18 +2,23 @@ package za.co.sabre.converter;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = ConverterDemoApplication.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ConverterTest
 {
 	private final HttpHeaders httpHeaders = new HttpHeaders();
-	private final TestRestTemplate testRestTemplate = new TestRestTemplate( );
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
 
 	@LocalServerPort
 	private int port;
@@ -22,26 +27,62 @@ public class ConverterTest
 	public void givenHectareValueCheckIfItConvertsToCorrectAcreValue(){
 		double hectareValue = 3.0;
 		HttpEntity<String> entity = new HttpEntity<>(null, httpHeaders);
-		ResponseEntity<Double> convertHectareEntity = testRestTemplate.exchange(createURLWithPort("/hectare/acre/" + hectareValue),
+		ResponseEntity<Double> convertToAcreEntity = testRestTemplate.exchange(createURLWithPort("/hectare/acre/" + hectareValue),
 				HttpMethod.GET,
 				entity,
 				Double.class);
 		Double expectedAcreValue = 1.21;
-		assertEquals(expectedAcreValue,convertHectareEntity.getBody());
-
+		assertEquals(expectedAcreValue,convertToAcreEntity.getBody());
 	}
 
 	@Test
 	public void givenHectareValueAndIncorrectAcreCheckIfItValidatesProperly(){
 		double hectareValue = 3.0;
 		HttpEntity<String> entity = new HttpEntity<>(null, httpHeaders);
-		ResponseEntity<Double> convertHectareEntity = testRestTemplate.exchange(createURLWithPort("/hectare/acre/" + hectareValue),
+		ResponseEntity<Double> convertToAcreEntity = testRestTemplate.exchange(createURLWithPort("/hectare/acre/" + hectareValue),
 				HttpMethod.GET,
 				entity,
 				Double.class);
 		Double incorrectAcreValue = 1.22;
-		assertNotEquals(incorrectAcreValue,convertHectareEntity.getBody());
+		assertNotEquals(incorrectAcreValue,convertToAcreEntity.getBody());
 	}
+
+	@Test
+	public void givenAcreValueCheckIfItConvertsToCorrectHectareValue(){
+		double acreValue = 3.0;
+		HttpEntity<String> entity = new HttpEntity<>(null, httpHeaders);
+		ResponseEntity<Double> convertToHectareEntity = testRestTemplate.exchange(createURLWithPort("/acre/hectare/" + acreValue),
+				HttpMethod.GET,
+				entity,
+				Double.class);
+		Double expectedHectareValue = 7.41;
+		assertEquals(expectedHectareValue,convertToHectareEntity.getBody());
+	}
+
+	@Test
+	public void givenAcreValueAndIncorrectHectareCheckIfItValidatesProperly(){
+		double acreValue = 3.0;
+		HttpEntity<String> entity = new HttpEntity<>(null, httpHeaders);
+		ResponseEntity<Double> convertToHectareEntity = testRestTemplate.exchange(createURLWithPort("/acre/hectare/" + acreValue),
+				HttpMethod.GET,
+				entity,
+				Double.class);
+		Double incorrectHectareValue = 7.0;
+		assertNotEquals(incorrectHectareValue,convertToHectareEntity.getBody());
+	}
+
+	@Test
+	public void givenCelsiusValueCheckIfItConvertsToCorrectFahrenheitValue(){
+		double celsiusValue = 42;
+		HttpEntity<String> entity = new HttpEntity<>(null, httpHeaders);
+		ResponseEntity<Double> convertToFahrenheitEntity = testRestTemplate.exchange(createURLWithPort("/celsius/fahrenheit/" + celsiusValue),
+				HttpMethod.GET,
+				entity,
+				Double.class);
+		Double expectedFahrenheitValue = 5.56;
+		assertEquals(expectedFahrenheitValue,convertToFahrenheitEntity.getBody());
+	}
+
 
 
 	private String createURLWithPort(String uri){
